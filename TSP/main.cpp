07c7dc65117272e0ed7d0cbb9f27be5549224b4e
Hayden Scott <HaydenScott573@gmail.com>
2016-01-22 14:01:57 -0700

    Initial Commit

---
+++ TSP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TSP
+//
+//  Created by Hayden Scott on 1/22/16.
+//  Copyright © 2016 Hayden Scott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
